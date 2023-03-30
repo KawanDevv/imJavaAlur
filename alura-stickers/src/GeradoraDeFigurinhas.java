833fc5571a13a4e06ecdf30ab79dd49f9d149114
KawanDevv <102386037+KawanDevv@users.noreply.github.com>
2023-03-30 11:01:51 +0000

    deleted:    README.md 	modified:   alura-stickers/saida/figurinha.png 	modified:   alura-stickers/src/App.java 	modified:   alura-stickers/src/GeradoraDeFigurinhas.java

--- alura-stickers/src/GeradoraDeFigurinhas.java
+++ alura-stickers/src/GeradoraDeFigurinhas.java
@@ -1,17 +1,26 @@
+import java.awt.Color;
+import java.awt.Font;
 import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.InputStream;
 
 import javax.imageio.ImageIO;
 
 public class GeradoraDeFigurinhas {
     
-    public void cria() throws Exception {
+    public void cria(InputStream inputStream, String nomeArquivo) throws Exception {
 
         // leitura da imagem
-        BufferedImage imagemOriginal = ImageIO.read(new File("alura-stickers/entrada/imgTeste.jpg"));
 
-        // cria nova imagem em memória com transparência e com tamanho novo
+        // InputStream inputStream = 
+        //              new FileInputStream(new File("alura-stickers/entrada/imgTeste.jpg"));
+        // InputStream inputStream = 
+        //              new URL("https://m.media-amazon.com/images/M/MV5BOWEwODJmZDItYTNmZC00OGM4LThlNDktOTQzZjIzMGQxODA4XkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_UX128_CR0,12,128,176_AL_.jpg")
+        //              .openStream();
+        BufferedImage imagemOriginal = ImageIO.read(inputStream);
+
+        // cria nova imagem em memória com transparência e com tamanho novo 
 
         int largura = imagemOriginal.getWidth();
         int altura = imagemOriginal.getHeight();
@@ -25,14 +34,21 @@ public class GeradoraDeFigurinhas {
         Graphics2D graphics = (Graphics2D) novaImagem.getGraphics();
         graphics.drawImage(imagemOriginal, 0, 0, null);
 
+
+        // configurar fonte
+
+        var fonte = new Font(Font.SANS_SERIF, Font.BOLD, 64);
+        graphics.setFont(fonte);
+        graphics.setColor(Color.YELLOW);
         // escrever uma mensagem na nova imagem
-        ImageIO.write(novaImagem, "png", new File("alura-stickers/saida/figurinha.png"));
+
+        graphics.drawString("TOPZERA", 100, novaAltura - 100);
+
+
         // escrever a nova imagem em um arquivo
 
-
+        ImageIO.write(novaImagem, "png", new File(nomeArquivo));
     }
-    public static void main(String[] args) throws Exception {
-        var geradora = new GeradoraDeFigurinhas();
-        geradora.cria();
-    }
+ 
+    
 }
